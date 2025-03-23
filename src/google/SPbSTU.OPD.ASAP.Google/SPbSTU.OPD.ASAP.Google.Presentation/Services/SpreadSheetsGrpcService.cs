b9e6eb6455633481b53aa71539533924b16c7437
Alex <alexandr.gradov@mail.ru>
2025-03-23 12:00:19 +0300

    add proto

---
+++ src/google/SPbSTU.OPD.ASAP.Google/SPbSTU.OPD.ASAP.Google.Presentation/Services/SpreadSheetsGrpcService.cs
@@ -0,0 +1,13 @@
+﻿using Grpc.Core;
+using SPbSTU.OPD.ASAP.Google.Presentation;
+
+namespace SPbSTU.OPD.ASAP.Google.Services;
+
+public class SpreadSheetsGrpcService : SpreadSheetsService.SpreadSheetsServiceBase
+{
+    // TODO: write grpc service
+    public override Task<CreateSpreadSheetsResponse> CreateSpreadSheets(CreateSpreadSheetsRequest request, ServerCallContext context)
+    {
+        return base.CreateSpreadSheets(request, context);
+    }
+}
