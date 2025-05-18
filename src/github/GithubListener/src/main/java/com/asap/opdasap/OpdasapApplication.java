03b39d965c0982d61d8ed53913cbd01d854fab40
Ilya <Ilya.boltyshev@mail.ru>
2025-05-18 23:25:17 +0300

    Invites

--- src/github/GithubListener/src/main/java/com/asap/opdasap/OpdasapApplication.java
+++ src/github/GithubListener/src/main/java/com/asap/opdasap/OpdasapApplication.java
@@ -2,7 +2,9 @@ package com.asap.opdasap;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication
 public class OpdasapApplication {
 
